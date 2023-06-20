93490bf34bcf4bf7b8e0f408393fc0609eebb415
chrisJohns0n <126682002+ChrisJohnson0110@users.noreply.github.com>
2023-06-20 21:59:29 +0100

    Weapons/items

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CJ
+{
+    public class PlayerInventory : MonoBehaviour
+    {
+        WeaponSlotManager WeaponSlotManagerRef;
+
+        public WeaponItem rightWeapon;
+        public WeaponItem leftWeapon;
+
+        private void Awake()
+        {
+            WeaponSlotManagerRef = GetComponentInChildren<WeaponSlotManager>();
+        }
+
+        private void Start()
+        {
+            WeaponSlotManagerRef.LoadWeaponOnSlot(rightWeapon, false);
+            WeaponSlotManagerRef.LoadWeaponOnSlot(leftWeapon, true);
+        }
+
+    }
+}
